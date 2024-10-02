9cd31e914c8ec073d9237a54c12defeb7af366f9
Quantus! <amazingalexe@me.com>
2024-10-02 17:35:14 +0100

    update

--- Learn C++ project/main.cpp
+++ Learn C++ project/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "main.h"
+#include <string>
 
 
 struct Simulation {
@@ -8,20 +8,22 @@ struct Simulation {
 	const double gravity{ 9.8 };
 	double distanceFallen{ 0 };
 	int cSecond{ 0 }; //each second is treated as a "cycle"/frame
-	int simulationDuration{ 5 };
-	
-};
-Simulation simulate(Simulation& sim) {
-	if (sim.cHeight <= 0) {
-		sim.cHeight = 0;
-		return sim;
+	int simulationDuration{ 6 };
+	void setHeight(double h) { 
+		startHeight = static_cast<int>(h);
+		cHeight = h;
 	}
-	sim.distanceFallen = sim.gravity * (sim.cSecond * sim.cSecond) / 2;
-	sim.cHeight = sim.startHeight - sim.distanceFallen;
-	sim.cSecond++;
-	return sim;
+};
+
+int getIntUserInput(std::string prompt) {
+	std::cout << prompt;
+	int input{};
+	std::cin >> input;
+	return input;
 }
-int simulaionPrinter(Simulation& sim) {
+
+
+int simulationPrinter(Simulation& sim) {
 	if (sim.cHeight <= 0) { std::cout << "At " << sim.cSecond << " seconds, the ball is on the ground." << std::endl; }
 	else {
 		std::cout << "At " << sim.cSecond << " seconds, the ball is at height: " << sim.cHeight << " meters" << std::endl;
@@ -29,18 +31,31 @@ int simulaionPrinter(Simulation& sim) {
 	}
  	return 0;
 }
+
+Simulation simulate(Simulation& sim) {
+	
+	if (sim.cHeight <= 0) {
+		sim.cHeight = 0;
+		return sim;
+	}
+	sim.distanceFallen = sim.gravity * (sim.cSecond * sim.cSecond) / 2;
+	sim.cHeight = sim.startHeight - sim.distanceFallen; //I initially just used cHeight = cHeight - distanceFallen, but as cSecond increased, the rate of decline
+	//exponentially?? increased, so i decided to use startHeight and a *constructor*?? on my stuct to initialise
+	// the two variables at the same time 
+	simulationPrinter(sim);
+	sim.cSecond++;
+	return sim;
+}
+
 int main()
 {
 	Simulation sim;
-	sim.cHeight = static_cast<double>(getIntUserInput("Please enter height of the tower in meters: "));
-	for (int i = 0; i < 5; i++) {
-		simulate(sim);
-		simulaionPrinter(sim);
-
-
+	sim.setHeight(static_cast<double>(getIntUserInput("Please enter height of the tower in meters: ")));
+	for (int i = 0; i < sim.simulationDuration; i++) {
+		simulate(sim); // I also initially had problems here as i was passing the whole sim object to my functions, instead of using a memory reference. 
+		 //I have no idea if return sim; actually does anything as i think simulate() just edits the values in memory instead of the function needing to be
+							//recopied (eg. in python, i would need to use sim = simulate(sim) )
 	}
-
-
 	return 0;
 }
 
